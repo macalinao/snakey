c8a30ba9ef2f179702c6ec0f41a92958ebe3edb4
Ian Macalinao <ianmacalinao@gmail.com>
2012-09-28 12:47:28 -0500

    Better segment management. Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/snake.c
+++ src/snake.c
@@ -2,18 +2,44 @@
 
 #include "snake.h"
 
+/**
+ * Creates a new segment.
+ * @param  x The x of the segment.
+ * @param  y The y of the segment.
+ * @return   The segment.
+ */
+static segment *segment_new(int x, int y) {
+    segment *s = (segment*) malloc(sizeof(segment));
+    s->x = x;
+    s->y = y;
+    return s;
+}
+
+/**
+ * Inserts a segment between the two given adjacent segments.
+ * This is ONLY for adjacent segments, unless you want a memory leak.
+ * 
+ * @param prev The segment to be previous.
+ * @param next The segment to be next.
+ * @param s    The segment to insert between prev and next.
+ */
+static void segment_insert(segment *prev, segment *next, segment *s) {
+    prev->next = s;
+    next->prev = s;
+
+    s->prev = prev;
+    s->next = next;
+}
+
 snake *snake_new(int x, int y) {
     snake *s = (snake*) malloc(sizeof(snake));
 
     // Snakes start out as 3 segments
-    segment *head = (segment*) malloc(sizeof(segment));
-    segment *middle = (segment*) malloc(sizeof(segment));
-    segment *tail = (segment*) malloc(sizeof(segment));
+    segment *head = segment_new(x, y);
+    segment *middle = segment_new(x + 1, y);
+    segment *tail = segment_new(x + 2, y);
 
-    head->next = middle;
-    middle->prev = head;
-    middle->next = tail;
-    tail->prev = middle;
+    segment_insert(head, tail, middle);
 
     s->head = head;
     s->tail = tail;
@@ -22,7 +48,11 @@ snake *snake_new(int x, int y) {
 }
 
 void snake_update(snake *snake) {
+    // Prepend a new segment based on the current head
+    segment *next = (segment*) malloc(sizeof(segment));
+    segment *head = snake->head;
 
+    next->next = head;
 }
 
 void snake_render(snake *snake) {
